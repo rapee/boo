b81037282d9c2d300569438b9d9784e2655def18
Rapee Suveeranont <iiieyes@gmail.com>
2016-05-15 22:12:34 +0700

    Changed deployment server to malacca Removed feathers-authentication Removed user service

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,13 @@
 'use strict';
 const render = require('./render');
-const authentication = require('./authentication');
-const user = require('./user');
+// const authentication = require('./authentication');
+// const user = require('./user');
 
 module.exports = function () {
   const app = this;
 
 
-  app.configure(authentication);
-  app.configure(user);
+  // app.configure(authentication);
+  // app.configure(user);
   app.configure(render);
 };
